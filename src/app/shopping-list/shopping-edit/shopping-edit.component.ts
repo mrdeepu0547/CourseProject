3a8f61ec51a1e75773b39b19f0d5753e7513f70e
mrdeepu0547 <saideepak.sabbineni2000@gmail.com>
2022-11-09 10:39:12 +0530

    shopping list form updates

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,8 +1,11 @@
 import {
   Component,
+  OnDestroy,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -12,16 +15,47 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.scss']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit ,OnDestroy{
+  @ViewChild('f') slForm:NgForm;
   constructor(private slService: ShoppingListService) { }
-
+ subscription:Subscription;
+ editMode=false;
+ editItemIndex:number;
+ editedItem:Ingredient;
   ngOnInit() {
+   this.subscription= this.slService.startedEditing.subscribe(
+    (index: number)=>{
+        this.editItemIndex=index;
+        this.editMode=true;
+        this.editedItem=this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name:this.editedItem.name,
+          amount: this.editedItem.amount
+        })
+    }
+   );
   }
 
   onAddItem(form:NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+        this.slService.updateIngredient(this.editItemIndex,newIngredient);
+    }else{
+      this.slService.addIngredient(newIngredient);
+    }
+    this.editMode=false;
+    form.reset();
   }
-
+  onClear(){
+    this.slForm.reset();
+    this.editMode=false;
+  }
+  onDelete(){
+    this.slService.deleteIngredient(this.editItemIndex);
+    this.onClear();
+  }
+   ngOnDestroy(){
+    this.subscription.unsubscribe();
+   }
 }
